c343cdc6b3e096d6bd401b0614ee6d6186fed411
Olsha100 <bolszewski1@gmail.com>
2022-11-14 22:52:31 +0100

    Add simple input form handling

--- app.js
+++ app.js
@@ -11,7 +11,13 @@ app.listen(PORT_NUMBER);
 console.log('Server is listening on port ' + PORT_NUMBER)
 
 
+
+// Define where static files are stored
+
 app.use(express.static('public'));
+
+
+/*******  GET requests ********/
 
 app.get('/', (req, res) => {
     res.sendFile('./pages/index.html', {root: __dirname});
@@ -19,4 +25,15 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
     res.sendFile('./pages/about.html', {root: __dirname});
+})
+
+/*******  POST requests ********/
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.text());
+
+app.post('/', (req,res) => {
+    console.log(req.body);
+    res.sendFile('./pages/thankYouPage.html', {root: __dirname});
 })
